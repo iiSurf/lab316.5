dea4574d6a2e109c8f962a10c942802bb92971e9
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-20 23:30:57 -0700

    I broke it

--- script.mjs
+++ script.mjs
@@ -6,3 +6,35 @@ myForm.addEventListener("submit", (e) => {
   ${myForm.querySelector("input").value}`);
   
 });
+
+const email = form.elements["email"];
+
+function validateEmail(evt) {
+  let emailVal = email.value;
+
+  const atpos = emailVal.indexOf("@");
+  const dotpos = emailVal.lastIndexOf(".");
+
+  if (atpos < 1) {
+    alert(
+      "Your email must include an @ symbol, which must not be at the beginning of the email."
+    );
+    email.focus();
+    evt.returnValue = false;
+    return false;
+  }
+
+  if (dotpos - atpos < 2) {
+    alert(
+      "Invalid structure: @.\nYou must include a domain name after the @ symbol."
+    );
+    email.focus();
+    evt.returnValue = false;
+    return false;
+  }
+
+  evt.returnValue = true;
+  return emailVal;
+}
+
+form.addEventListener("submit", validateEmail);
